d6e25417ffce92770f7694d44494d46eae04049e
Riku <rikusugawara.1103@gamil.com>
2025-03-07 13:51:30 +0900

    1-6

---
+++ src/resources/views/hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>ブラウザに画面を表示しよう（復習）</h2>
+    <ul>
+        <li>ビューを作成する</li>
+        <li>コントローラーでビューを呼び出す</li>
+        <li>ルーティングを設定する</li>
+    </ul>
+</body>
+
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index']);
